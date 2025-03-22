f4a9c91b1bdc74e337b05f0dae254c4ff3aa9bfb
Nicolò <nicopaltrinieri2004@gmail.com>
2025-03-22 19:24:34 +0100

    primo commit

---
+++ fantasy.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+
+#define n_piloti 5
+#define n_squadre 2
+
+#define dim_piloti 20
+#define dim_squadre 10
+
+extern void fantasy(double* p_piloti, double* p_squadre);
+
---
+++ main.c
@@ -0,0 +1,11 @@
+#include"fantasy.h"
+
+int main(void) {
+	
+	double p_piloti[] = { 29,23,25.9,24.2,28.4,18,21,18.4,13.1,12,11.8,7.8,8.8,8.1,9.6,6.2,7.3,6.7,6.4,6 };
+	double p_squadre[] = { 30,27.1,25.2,22.7,13.1,9.5,8.5,8,7,6.2 };
+	
+	fantasy(p_piloti, p_squadre);
+
+	return;
+}
